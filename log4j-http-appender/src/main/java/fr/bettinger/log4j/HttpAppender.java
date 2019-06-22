670c157923a5cab74daafbc51676d648af9613ee
Sandeep Kumar <kumar-sand@HCL.COM>
2019-06-22 18:02:22 +0530

    my first commit

---
+++ log4j-http-appender/src/main/java/fr/bettinger/log4j/HttpAppender.java
@@ -0,0 +1,203 @@
+/*
+ *   This file is part of Log4jHttpAppender.
+ *
+ *   Log4jHttpAppender is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Log4jHttpAppender is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *   You should have received a copy of the Lesser GNU General Public License
+ *   along with Log4jHttpAppender.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ *   The original code was written by Sebastien Bettinger <sebastien.bettinger@gmail.com>
+ *   
+ */
+
+package fr.bettinger.log4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.LogLog;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.io.*;
+import org.apache.logging.log4j.core.*;
+import org.apache.logging.log4j.core.config.plugins.*;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+
+
+import fr.bettinger.log4j.HttpLayout.URLParameterNameLayout;
+
+/**
+ * HttpAppender is used to send an httpEvent to a server
+ * This appender must be used with a HttpLayout type
+ * 
+ * @author S. Bettinger
+ *
+ */
+@Plugin(name="HttpAppender", category="Core", elementType="appender", printObject=true)
+public class HttpAppender extends AbstractAppender {
+	final Logger log = Logger.getLogger(HttpAppender.class);
+
+	/**
+	 * Valeur par défaut du Timeout de la connexion http
+	 */
+	private final static int DEFAULT_TIMEOUT = 0;
+
+	
+	private final static String METHOD_GET = "GET";
+	private final static String METHOD_POST = "POST";
+	/**
+	 * Method par defaut POST/GET
+	 */
+	private final static String DEFAULT_METHOD = METHOD_GET;
+
+	private final static String POST_PARAMETERS = "PARAMETERS";
+	private final static String POST_QUERY_STRING = "QUERY_STRING";
+	/**
+	 * Passage de paramètre par défaut pour la méthode post
+	 */
+	private final static String POST_DEFAULT = POST_PARAMETERS;
+
+	/**
+	 * Valeur par défaut de la connexion par thread 
+	 */
+	private final static boolean THREAD_DEFAULT = true;
+	
+	/**
+	 * Gestion du timeout de la connexion HTTP
+	 */
+	private int timeOut = DEFAULT_TIMEOUT;
+	
+	/**
+	 * URL d'appel du logger
+	 */
+	private String logURL = null;
+
+	private String HttpMethodBase = DEFAULT_METHOD;
+
+	private String postMethod = POST_DEFAULT;
+	
+	private boolean thread = THREAD_DEFAULT;
+	
+	private ExecutorService executorService = Executors.newCachedThreadPool();
+
+	public void close() {
+		executorService.shutdown();
+	}
+
+	public boolean requiresLayout() {
+		return true;
+	}
+
+	
+	protected HttpAppender(String name, Filter filter, Layout<? extends Serializable> layout) {
+        super(name, filter, layout);
+    }	
+	
+	@Override
+	protected void append(LoggingEvent paramLoggingEvent) {
+		/*
+		 * Dans le cas ou le layout n'est pas de type HttpLayout, on ne fait rien
+		 */
+		if (!(this.getLayout() instanceof HttpLayout)) {
+			errorHandler.error("you must use a HttpLayout type");
+			return;
+		}
+		HttpLayout layout = (HttpLayout) getLayout();
+		
+		HttpMethodBase httpMethod = null;
+		HttpClient httpClient = new HttpClient();
+		/*
+		 * On positionne le timeout
+		 */
+		httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(timeOut);
+		httpClient.getParams().setSoTimeout(timeOut);		
+
+		if (this.HttpMethodBase.equalsIgnoreCase(METHOD_GET)) {
+			String query = layout.format(paramLoggingEvent);
+			String url = this.logURL + query;
+			LogLog.debug(url);
+			httpMethod = new GetMethod(url);
+		} else {
+			if (this.postMethod.equalsIgnoreCase(POST_PARAMETERS)) {
+				httpMethod = new PostMethod(this.logURL);
+				// post requests don't need to be encoded manually
+				layout.urlEncode = false;
+				for (int i = 0; i < layout.subLayouts.size(); i+=2) {
+					URLParameterNameLayout nameLayout = (URLParameterNameLayout) layout.subLayouts.get(i);
+					String value = layout.subLayouts.get(i + 1).format(paramLoggingEvent);
+					((PostMethod) httpMethod).addParameter(nameLayout.getValue(), value);
+				}
+			} else {
+				String message = layout.format(paramLoggingEvent);
+				StringBuffer sb = new StringBuffer(this.logURL);
+				sb.append(message);
+				httpMethod = new PostMethod(sb.toString());
+			}
+		}
+		HttpThread httpThread = new HttpThread(httpClient,errorHandler);
+		httpThread.setMethod(httpMethod);
+
+		if (thread) {
+			executorService.submit(httpThread);
+		} else {
+			httpThread.run();
+		}
+	}
+	
+	/*
+	 * Getter
+	 */
+	public void setPost(String method) {
+		this.postMethod = method;
+	}
+
+	public void setMethod(String method) {
+		this.HttpMethodBase = method;
+	}
+
+	public void setLogURL(String logURL) {
+		this.logURL = logURL;
+	}
+	
+	public void setThread(boolean b) {
+		this.thread = b;
+	}
+
+	public void setTimeout(int to) {
+		this.timeOut = to;
+	}
+	
+	
+	@PluginFactory
+    public static HttpAppender createAppender(
+            @PluginAttribute("name") String name,
+            @PluginElement("Layout") Layout<? extends Serializable> layout,
+            @PluginElement("Filter") final Filter filter,
+            @PluginAttribute("host") String host) {
+        if (name == null) {
+            LOGGER.error("No name provided for HttpAppender");
+            return null;
+        }
+        if (layout == null) {
+            layout = PatternLayout.createDefaultLayout();
+        }
+		HttpAppender temp = new HttpAppender(name, filter, layout, true);
+		temp.setLogURL(host);
+        return temp;
+    }	
+}
---
+++ log4j-http-appender/src/main/java/fr/bettinger/log4j/HttpLayout.java
@@ -0,0 +1,160 @@
+/*
+ *   This file is part of Log4jHttpAppender.
+ *
+ *   Log4jHttpAppender is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Log4jHttpAppender is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *   You should have received a copy of the Lesser GNU General Public License
+ *   along with Log4jHttpAppender.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ *   The original code was written by Sebastien Bettinger <sebastien.bettinger@gmail.com>
+ *   
+ */
+
+package fr.bettinger.log4j;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.EnhancedPatternLayout;
+import org.apache.log4j.Layout;
+import org.apache.log4j.helpers.LogLog;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * HttpLayout 
+ * @author S. Bettinger
+ *
+ */
+public class HttpLayout extends Layout {
+	private final class URLEncodingPatternLayout extends EnhancedPatternLayout {
+		private URLEncodingPatternLayout(String pattern) {
+			super(pattern);
+		}
+
+		@Override
+		public String format(LoggingEvent event) {
+			String value = super.format(event);
+
+			if (urlEncode) {
+				try {
+					value = URLEncoder.encode(value, encoding);
+				} catch (UnsupportedEncodingException e) {
+					LogLog.warn(e.toString());
+				}
+			}
+
+			return value;
+		}
+	}
+	
+	public static class URLParameterNameLayout extends Layout {
+		private final String type;
+		private final String value;
+		
+		public URLParameterNameLayout(String type, String value) {
+			super();
+			this.type = type;
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		@Override
+		public void activateOptions() {
+			
+		}
+		
+		@Override
+		public boolean ignoresThrowable() {
+			return true;
+		}
+		
+		@Override
+		public String format(LoggingEvent event) {
+			return type + value + "=";
+		}
+	}
+
+
+	public static final Pattern URL_PARAMETER_PATTERN = Pattern.compile("(\\?|&)(\\w+)=");
+	
+	List<Layout> subLayouts = new ArrayList<Layout>();
+	
+	public HttpLayout() {
+		
+	}
+	
+	public HttpLayout(String pattern) throws MalformedURLException {
+		setConversionPattern(pattern);
+	}
+	
+	public void setConversionPattern(String conversionPattern) throws MalformedURLException {
+		Matcher m = URL_PARAMETER_PATTERN.matcher(conversionPattern);
+		if(!m.find() || m.start() != 0 || !m.group(1).equals("?")) {
+			throw new MalformedURLException("You need to provide a proper query string starting with ?");
+		}
+		
+		for(;;) {
+			subLayouts.add(new URLParameterNameLayout(m.group(1), m.group(2)));
+			int end = m.end();
+			if(!m.find()) {
+				subLayouts.add(new URLEncodingPatternLayout(conversionPattern.substring(end)));
+				return;
+			}
+			subLayouts.add(new URLEncodingPatternLayout(conversionPattern.substring(end, m.start())));
+		}
+	}
+
+	public String encoding = "UTF-8";
+	public void setEncoding(String encoding) throws UnsupportedEncodingException {
+		if(encoding == null || !Charset.isSupported(encoding)) {
+			throw new UnsupportedEncodingException(encoding);
+		}
+		this.encoding = encoding.trim();
+	}
+
+	public boolean urlEncode = true;
+	public void setUrlEncode(boolean urlEncode) {
+		this.urlEncode = urlEncode;
+	}
+
+	@Override
+	public void activateOptions() { }
+
+	@Override
+	public String format(LoggingEvent paramLoggingEvent) {
+		StringBuilder builder = new StringBuilder();
+		
+		for (Layout layout : subLayouts) {
+			builder.append(layout.format(paramLoggingEvent));
+		}
+		
+		return builder.toString();
+	}
+
+	@Override
+	public boolean ignoresThrowable() {
+		for (Layout layout : subLayouts) {
+			if(!layout.ignoresThrowable()) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
